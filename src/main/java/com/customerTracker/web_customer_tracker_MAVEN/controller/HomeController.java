4561b5be3b7120d0474429d09210871ecb3013fe
Alejandro Lopez <alejandro_lopez207@hotmail.com>
2018-02-15 23:41:34 -0600

    Second commit

--- src/main/java/com/customerTracker/web_customer_tracker_MAVEN/controller/HomeController.java
+++ src/main/java/com/customerTracker/web_customer_tracker_MAVEN/controller/HomeController.java
@@ -6,13 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
+	public String test(HttpServletResponse response) throws IOException{
+		//return new ModelAndView("home");
+		return "home";
 	}
 }
